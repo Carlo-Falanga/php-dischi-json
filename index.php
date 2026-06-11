832778f7fe331c441dc6dc20be2f800a55d44748
Carlo-Falanga <carlofalanga7@gmail.com>
2026-06-11 10:21:17 +0200

    Add initial PHP files for music records handling

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
+<?php
+
+$json_string = file_get_contents("./musicRecords.json");
+// var_dump($file);
+
+$musicRecords = json_decode($json_string);
+var_dump($musicRecords);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Dischi</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
